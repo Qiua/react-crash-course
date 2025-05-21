7c3b2c61a7e0865fdd00653c8bb1a9959b0f1d6b
Andrey Aires <andrey.aires@gmail.com>
2025-05-21 20:15:48 -0300

    aula-4: Styling with css modules

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ function App() {
 
   return (
     <main>
-      <Post author="Andrey" body="React makes building user interfaces a breeze! I'm learning how components work." />
-      <Post author="Sarah" body="Just deployed my first React application. Hooks are amazing!" />
-      <Post author="Mike" body="Props and state management are crucial concepts in React." />
-      <Post author="Emily" body="Virtual DOM is what makes React so efficient and fast." />
+      <Post title="Title 1" author="Andrey" body="React makes building user interfaces a breeze! I'm learning how components work." />
+      <Post title="Title 2" author="Sarah" body="Just deployed my first React application. Hooks are amazing!" />
+      <Post title="Title 3" author="Mike" body="Props and state management are crucial concepts in React." />
+      <Post title="Title 4" author="Emily" body="Virtual DOM is what makes React so efficient and fast." />
     </main>
   )
 }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,11 @@
+import classes from "./Post.module.css";
+
 function Post(props) {
   return (
-    <div className="post">
-      <h2>{props.author} chegou!</h2>
-      <p>{props.body}</p>
+    <div className={classes.post}>
+      <h1 className={classes.title}>{props.title}</h1>
+      <h2 className={classes.author}>{props.author} disse:</h2>
+      <p className={classes.text}>{props.body}</p>
     </div>
   );
 }
