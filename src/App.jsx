cccf6e0e75d6d288acb942e999475b36b0cdd7f8
Andrey Aires <andrey.aires@gmail.com>
2025-05-21 18:28:06 -0300

    Initial Build

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Hello World!👌</h1>
+}
+
+export default App
---
+++ src/components/Post.jsx
@@ -0,0 +1,10 @@
+function Post() {
+  return (
+    <div className="post">
+      <h2>Post Title</h2>
+      <p>This is a sample post content.</p>
+    </div>
+  );
+}
+
+export default Post;
