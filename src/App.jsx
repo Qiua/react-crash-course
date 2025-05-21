fb68dcdba22fd3c3739e8a8107fccc97d76bcb08
Andrey Aires <andrey.aires@gmail.com>
2025-05-21 20:17:23 -0300

    aula-4.1: Adding New Titles

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ function App() {
 
   return (
     <main>
-      <Post title="Title 1" author="Andrey" body="React makes building user interfaces a breeze! I'm learning how components work." />
-      <Post title="Title 2" author="Sarah" body="Just deployed my first React application. Hooks are amazing!" />
-      <Post title="Title 3" author="Mike" body="Props and state management are crucial concepts in React." />
-      <Post title="Title 4" author="Emily" body="Virtual DOM is what makes React so efficient and fast." />
+      <Post title="React Journey Begins" author="Andrey" body="React makes building user interfaces a breeze! I'm learning how components work." />
+      <Post title="My First Deployment" author="Sarah" body="Just deployed my first React application. Hooks are amazing!" />
+      <Post title="Understanding React Core" author="Mike" body="Props and state management are crucial concepts in React." />
+      <Post title="Performance Insights" author="Emily" body="Virtual DOM is what makes React so efficient and fast." />
     </main>
   )
 }
