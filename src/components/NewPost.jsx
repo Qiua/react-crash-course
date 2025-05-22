6d26f85579fbb8c8bc5cce19270d59aedb04ee43
Andrey Aires <andrey.aires@gmail.com>
2025-05-22 16:12:01 -0300

    aula-6.4: Destructuring props

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,17 +1,17 @@
 import classes from './NewPost.module.css';
 
-function NewPost(props) {
+function NewPost({onBodyChange, onAuthorChange}) {
     
 
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={props.onBodyChange}/>
+        <textarea id="body" required rows={3} onChange={onBodyChange}/>
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required onChange={props.onAuthorChange} />
+        <input type="text" id="name" required onChange={onAuthorChange} />
       </p>
     </form>
   );
