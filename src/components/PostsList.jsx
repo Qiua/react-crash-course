c06c23affce089d893caf4d43e09ca1f99dc3c48
Andrey Aires <andrey.aires@gmail.com>
2025-05-22 11:23:58 -0300

    aula-6: Preparing for EventListeners

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,8 +1,13 @@
 import classes from "./PostsList.module.css";
 import Post from "./Post";
+import NewPost from './NewPost'
 
 function PostsList() {
   return (
+    <>
+    <h2>New Post</h2>
+    <NewPost />
+    <h2>All Posts</h2>
     <ul className={classes.posts}>
         <Post title="Learning React" author="John" body="Today I started learning React. It's so exciting!" />
         <Post title="React Journey Begins" author="Andrey" body="React makes building user interfaces a breeze! I'm learning how components work." />
@@ -10,6 +15,7 @@ function PostsList() {
         <Post title="Understanding React Core" author="Mike" body="Props and state management are crucial concepts in React." />
         <Post title="Performance Insights" author="Emily" body="Virtual DOM is what makes React so efficient and fast." />
     </ul>
+    </>
   );
 }
 
