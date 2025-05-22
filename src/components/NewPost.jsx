cf8d54868711fa4ed1b7698635673d50613da2bc
Andrey Aires <andrey.aires@gmail.com>
2025-05-22 12:20:22 -0300

    aula-6.2: React State Feature

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,8 +1,12 @@
+import { useState } from 'react';
+
 import classes from './NewPost.module.css';
 
 function NewPost() {
+    const [enteredText, setEnteredText] = useState('');
+
     function changeBodyHandler(event) {
-        console.log(event.target.value);
+        setEnteredText(event.target.value);
     }
   return (
     <form className={classes.form}>
@@ -10,6 +14,7 @@ function NewPost() {
         <label htmlFor="body">Text</label>
         <textarea id="body" required rows={3} onChange={changeBodyHandler}/>
       </p>
+      <p>{enteredText}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
